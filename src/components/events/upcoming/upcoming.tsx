2fa23c72ff89a33a993481b84554b9d0493bdfdb
Surajphirke3 <surajphirke3@gmail.com>
2025-09-01 17:35:20 +0530

    feat: Add initial components for contacts, events, home, socials, and team

---
+++ src/components/events/upcoming/upcoming.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const upcoming = () => {
+  return (
+    <div>upcoming</div>
+  )
+}
+
+export default upcoming
---
+++ src/components/team/BE/BE.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BE = () => {
+  return (
+    <div>BE</div>
+  )
+}
+
+export default BE
