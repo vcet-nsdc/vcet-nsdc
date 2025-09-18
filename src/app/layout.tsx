691a2662c5407cf6686bb5aca309ffc6d1814d3a
Surajphirke3 <surajphirke3@gmail.com>
2025-09-19 02:43:45 +0530

    feat: Add styles for certificate modal and prevent body scroll when open

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -129,8 +129,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/manifest.json" />
-        
+      
         {/* DNS prefetch for performance */}
         <link rel="dns-prefetch" href="//fonts.googleapis.com" />
         <link rel="dns-prefetch" href="//ik.imagekit.io" />
