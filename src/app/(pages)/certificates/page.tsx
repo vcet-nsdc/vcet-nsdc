20779b08ab37515125fe16d5b23fbbee09ead164
Surajphirke3 <surajphirke3@gmail.com>
2025-09-19 02:46:42 +0530

    feat: Add certificate search and generation pages with functionality for retrieving and downloading certificates

---
+++ src/app/(pages)/certificates/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Certificate from '@/components/certificates/certificate'
+
+const Page = () => {
+  return (
+    <div>
+      {/* <Certificate/> */}
+    </div>
+  )
+}
+
+export default Page
